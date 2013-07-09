9fca2c80667f2a10c8e706f76894cd12a5279e8e
Enocre Shao <encore.shao@gmail.com>
2013-07-09 16:26:40 +0800

    MISC - Error spell

--- lib/linked_in2/api.rb
+++ lib/linked_in2/api.rb
@@ -1,5 +1,5 @@
 module LinkedIn2
-  module Api
+  module API
     autoload :QueryMethods,  "linked_in2/api/query_methods"
     autoload :UpdateMethods, "linked_in2/api/update_methods"
     autoload :Request,       "linked_in2/api/request"
